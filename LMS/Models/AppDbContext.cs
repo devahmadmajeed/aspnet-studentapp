d18de3d4a20b98e2d0458a77ba55273afcc14966
Ahmad Majeed <devahmadmajeed@gmail.com>
2026-04-06 16:52:06 +0500

    Initial commit: ASP.NET Student App source

---
+++ LMS/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace StudentApp.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("StudentDBContext") { }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
